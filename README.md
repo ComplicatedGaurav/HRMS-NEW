e386863dea06753f2449688cd547c3f5bcaa48b7
Pankaj Yadav <vvastcore@gmail.com>
2024-10-20 14:10:58 +0530

    wrapupp hrms

---
+++ README.md
@@ -0,0 +1,22 @@
+# Human Resource Management System
+
+[**Live Demo**]()
+
+A comprehensive software solution to efficiently manage employee-related tasks.
+
+## Key Features
+
+- User authentication (Login/Register)
+- Integrated Payroll management
+- CRUD functionality for employee records
+- Advanced search and filtering options for employees
+- Leave, loan, and bonus applications
+- Centralized ticket management
+- Intuitive admin dashboard with real-time statistics
+- Direct reminders integration with Google Calendar
+
+## Installation Steps
+
+1. Clone the repository:  
+   ```bash
+   git clone https://github.com/ComplicatedGaurav/HRMS-NEW.git
---
+++ client/src/components/layouts/Header.jsx
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Consumer } from "../../context";
+import "../../assets/header-styles/header.css";
+import { RiLogoutCircleLine } from "react-icons/ri";
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      deleteAccount: false,
+    };
+  }
+
+  OnLogout = (dispatch) => {
+    localStorage.setItem("auth-token", "");
+    localStorage.setItem("userId", "");
+    console.log("Logged out!");
+
+    dispatch({
+      type: "LOGGED_OUT",
+    });
+  };
+
+  getInfo = (todos) => {
+    let completed = 0;
+    todos.forEach((todoItem) => {
+      if (todoItem.finished) completed++;
+    });
+    return completed;
+  };
+
+  render() {
+    const { branding } = this.props;
+
+    return (
+      <Consumer>
+        {(value) => {
+          let { dispatch, token, user, todos } = value;
+
+          if (token === undefined) token = "";
+          if (user === undefined) user = "";
+          if (todos === undefined) todos = [];
+
+          // getting token from localstorage for removing delay
+          const localToken = localStorage.getItem("auth-token");
+
+          return (
+            <>
+              <nav className="navbar custom-navbar navbar-expand-lg navbar-light">
+                <div className="" style={{display:"flex", gap:'50%', width:'100%'}}>
+                  <Link to="/" className="navbar-brand brand-text">
+                    {branding}
+                  </Link>
+
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <i className="fa fa-bars"></i>
+                  </button>
+
+                  <div
+                    className="collapse navbar-collapse justify-content-end"
+                    id="navbarNavAltMarkup"
+                  >
+                    <div className="navbar-nav">
+                      {/* About link */}
+                      {
+                        !localToken &&
+                        (
+                          <Link to="/about" className="nav-link custom-nav-link">
+                            About
+                          </Link>
+                        )}
+
+                      {/* Conditionally render based on token */}
+                      {localToken ? (
+                        <>
+                          {/* Profile or Admin based on user role */}
+                          <Link
+                            to={user && user.role !== "admin" ? "/profile" : "#"}
+                            className="nav-link custom-nav-link"
+                          >
+                            Welcome back,   {user.name}!
+                          </Link>
+
+                          <span
+                            onClick={this.OnLogout.bind(this, dispatch)}
+                            className="nav-link custom-nav-link"
+                          style={{cursor:'pointer'}}
+                          >
+                            <RiLogoutCircleLine />
+                          </span>
+                        </>
+                      ) : (
+                        <>
+                          {/* SignUp and Login links with CTA button styles */}
+                          <Link to="/signup" className="nav-link custom-nav-link btn-cta">
+                            SignUp
+                          </Link>
+                          <Link to="/login" className="nav-link custom-nav-link btn-cta btn-outline">
+                            Login
+                          </Link>
+                        </>
+                      )}
+
+
+                      {/* Contact Us */}
+                      {
+                        !localToken &&
+                        (
+                          <Link to="/contactUs" className="nav-link custom-nav-link">
+                            Contact Us
+                          </Link>
+                        )
+                      }
+
+                    </div>
+                  </div>
+                </div>
+              </nav>
+            </>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default Header;
